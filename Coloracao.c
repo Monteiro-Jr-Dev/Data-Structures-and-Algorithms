a51581a756c0cd5aba623d11258995694e942cf9
Valmir Monteiro <valmirmjunior@gmail.com>
2019-07-27 11:23:24 -0300

    First Commit

---
+++ Coloracao.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListaBase.h"
+
+// Declaração de Funções
+int ComparaInserir (void* parametro1, void* parametro2);
+int ComparaBusca (void* parametro1, void* parametro2);
+void PreencherLista(FILE* grafo, int vertices, Lista* listaCandidatos);
+void ColorirVertices(Lista* listaCandidatos);
+int Adjacente(Lista* listaCor, Node* noComparar);
+void ImprimirCor(void* PontDados);
+void AtualizarListaCandidatos(Lista* listaCandidatos, Lista* listaCor);
+int RemoverNoColorido(Lista* lista, void* PDados);
+int _procurarPosicaoNoColorido(Lista* lista, void* dado);
+
+int main(){
+
+	// Declaração de variáveis
+	FILE* grafo; 
+	Lista* listaCandidatos = CriarLista(ComparaInserir, ComparaBusca);
+	int qVertice, qAresta;
+	char nomeGrafo[20];
+
+	// Solicitar o arquivo ao usuário
+	do{
+		printf("Nome de um arquivo valido: ");
+		scanf("%s", nomeGrafo);
+		grafo = fopen(nomeGrafo,"r");
+	} while(!grafo);
+
+	// Ler |V| e |E| 
+	fscanf(grafo, "%i %i", &qVertice, &qAresta);
+
+	printf("Coloracao: \n");
+	PreencherLista(grafo, qVertice, listaCandidatos);
+	ColorirVertices(listaCandidatos);
+
+	DestruirLista(listaCandidatos);
+	fclose(grafo);
+	return 0;
+}
+
+// Ordenar vértices de forma decrescente pelo grau
+int ComparaInserir (void* parametro1, void* parametro2){
+	int* vetor1 = (int*)parametro1;
+	int* vetor2 = (int*)parametro2;
+
+	if(vetor1[1] > vetor2[1]){
+		return 1;
+	} else if(vetor1[1] < vetor2[1]){
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+// Busca vértices com base no índice
+int ComparaBusca (void* parametro1, void* parametro2){
+	int* vetor1 = (int*)parametro1;
+	int* vetor2 = (int*)parametro2;
+
+	if(vetor1[0] > vetor2[0]){
+		return 1;
+	} else if(vetor1[0] < vetor2[0]){
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+void PreencherLista(FILE* grafo, int vertices, Lista* listaCandidatos){
+	// Declaração
+	int grauVertice, aresta;	
+
+	for (int i = 0; i < vertices; i++){
+		grauVertice = 0;
+
+		//Criar vetores que formam a lista
+		int* vetor = (int*) malloc(sizeof(int) * (vertices + 2));
+		
+		
+
+		for(int j = 0; j < vertices; j++){
+			// Offset de 2 pois as duas primeiras posições do vetor são reservadas para armazenar o índice e grau
+			fscanf(grafo, "%i", &vetor[j + 2]);
+			if(vetor[j + 2] == 1){
+				grauVertice++;
+			}
+		}
+
+		// Posição 0 reservada para o índice do vértice (Identificação)
+		vetor[0] = i; 
+
+		// Posição 1 reservada para o grau do vértice (Ordenação)
+		vetor[1] = grauVertice; 
+		AdicionarNoOrdenado(listaCandidatos, (void*)vetor);
+	}
+}
+
+void ColorirVertices(Lista* listaCandidatos){
+	int qVertices;
+	Node *primeiroNo, *noAtual;
+	Lista* listaCor;
+
+	// Verifica se a lista de candidatos tem elementos
+	if(qVertices = _contagemLista(listaCandidatos)){
+		listaCor = CriarLista(ComparaInserir, ComparaBusca);
+		// Insere o primeiro vértice dos candidatos na lista da cor
+		primeiroNo = noAtual = listaCandidatos->primeiroNo; 
+		AdicionarNo(listaCor, primeiroNo->PontDados);
+		
+		// Percorre a lista de candidatos procurando vértices não adjacentes 
+		// aos vértices já inseridos na lista de cor
+		for (int i = 1; i < qVertices; i++){
+			 noAtual = noAtual->proximo;
+			 if(!Adjacente(listaCor, noAtual)){
+			 	AdicionarNo(listaCor, noAtual->PontDados);			 	
+			 }
+		}
+		// Imprime os vértices que tem a mesma cor
+		printf("{ ");
+		AtravessarLista(listaCor, ImprimirCor); 
+		printf("}\n");
+
+		// Remove vértices já coloridos da lista de candidatos
+		AtualizarListaCandidatos(listaCandidatos, listaCor); 
+
+		// Repete a coloração nos candidatos restantes
+		ColorirVertices(listaCandidatos);
+	}
+}
+
+// Verifica se um vértice é adjacente a algum vértice da lista
+int Adjacente(Lista* listaCor, Node* noComparar){
+	int *vertice, *verticeComp, indiceComp;
+	int tamLista = _contagemLista(listaCor);
+	verticeComp = (int*)(noComparar->PontDados);
+	indiceComp = verticeComp[0];
+	Node* noAtual = listaCor->primeiroNo;
+
+	for (int i = 0; i < tamLista; i++){
+		vertice = (int*)(noAtual->PontDados);
+		// Se existe um vértice na lista que é adjacente 
+		if(vertice[indiceComp + 2] == 1){
+			return 1;
+		}
+		noAtual = noAtual->proximo;
+	}
+	return 0;
+}
+
+void ImprimirCor(void* PontDados){
+	int* vertice = (int*)PontDados;
+	printf("%i ", vertice[0] + 1); // +1 só para os vértices começarem em 1
+}
+
+// Remove vértices já coloridos da lista de candidatos
+void AtualizarListaCandidatos(Lista* listaCandidatos, Lista* listaCor){
+	int tamLista = _contagemLista(listaCor);
+	Node* noAtual = listaCor->primeiroNo;
+
+	for (int i = 0; i < tamLista; i++){
+		// Como o ponteiro já está em outra lista, não há necessidade de armazenar o retorno
+		RemoverNo(listaCandidatos, noAtual->PontDados);
+		noAtual = noAtual->proximo;
+	}
+	DestruirLista(listaCor);
+}
---
+++ ListaBase.h
@@ -0,0 +1,228 @@
+/*IFF Campos
+* Estruturas de Dados Avançada
+* Lista Base 
+*/
+
+// Estrutura do Nó da Lista
+typedef struct node{
+	void* PontDados;
+	struct node* proximo;
+}Node;
+
+// Estrutura do Cabeçalho da Lista
+typedef struct lista{
+	int cont;
+	Node* primeiroNo;
+	int (*ComparaInserir)(void* parametro1, void* parametro2); // Função comparação para inserção	
+	int (*ComparaBusca)(void* parametro1, void* parametro2); // Função de comparação para buscas
+
+}Lista;
+
+// Funções Básicas da Lista
+Lista* CriarLista(int (*ComparaInserir)(void* parametro1, void* parametro2),
+int (*ComparaBusca)(void* parametro1, void* parametro2));
+void DestruirLista(Lista* lista);
+int AdicionarNo(Lista* lista, void* ponteiroDados);
+void* RemoverNo(Lista* lista, void* PDados);
+void AtravessarLista(Lista* lista, void (*Funcao)(void* PtrDados));
+void AdicionarNoOrdenado(Lista* lista, void* ponteiroDados);
+
+int _procurarPosicaoNo(Lista* lista, void* dadoBuscar);			//Retorna posiçao do nó na lista
+Node* _procurarNoAnterior(Lista* lista, int posicao);			//Retorna o ponteiro para o nó anterior ao selecionado
+Node* _procurarUltimoNo(Lista* lista);
+void _destruirNo(Node* no);
+int _listaVazia(Lista* lista);
+int _contagemLista(Lista* lista);
+
+
+Lista* CriarLista(int (*ComparaInserir)(void* parametro1, void* parametro2),
+	int (*ComparaBusca)(void* parametro1, void* parametro2)){
+	Lista* lista = (Lista*) malloc(sizeof(Lista));
+	lista->cont = 0;
+	lista->primeiroNo = NULL;
+	lista->ComparaInserir = ComparaInserir;
+	lista->ComparaBusca = ComparaBusca;
+	return lista;
+}
+
+void DestruirLista(Lista* lista){
+
+	// Verifica se a lista está vazia
+	if(!_listaVazia(lista)){
+		Node *no, *proximoNo;
+		no = lista->primeiroNo;
+
+		// Libera todos os nós
+		while(no != NULL){
+			proximoNo = no->proximo;
+			_destruirNo(no);
+			no = proximoNo;
+		}
+	}
+	// Libera a lista
+	free(lista);
+}
+
+int AdicionarNo(Lista* lista, void* ponteiroDados){
+	// Cria e inicializa do nó
+	Node *novoNo = (Node*) malloc(sizeof(Node));
+	Node* ultimoNo;
+	novoNo->PontDados = ponteiroDados;
+	novoNo->proximo = NULL;	
+
+	if(_contagemLista(lista) == 0){
+		 lista->primeiroNo = novoNo;
+
+	} else{
+		ultimoNo = _procurarUltimoNo(lista);
+		ultimoNo->proximo = novoNo;		
+	}
+	(lista->cont)++;
+}
+
+// Remove o nó e retorna o ponteiro do dado
+void* RemoverNo(Lista* lista, void* PDados){
+	Node *noAnterior, *no, *noPosterior;
+	int posNo;
+	void* dadoNo = NULL;
+	// Encontrar a posição do nó usando a função de comparação
+	posNo = _procurarPosicaoNo(lista, PDados);	
+
+	// Se não encontrar o nó
+	if(posNo == 0)
+		return dadoNo; // dadoNo ainda é NULL
+
+	//Se o nó buscado for o primeiro, então o nó anterior é o cabeçalho
+	if(posNo == 1){ 
+		no = lista->primeiroNo;
+		noPosterior = no->proximo;
+		lista->primeiroNo = noPosterior;
+
+	} else{
+		noAnterior = _procurarNoAnterior(lista, posNo);
+		no = noAnterior->proximo;
+		noPosterior = no->proximo;
+		noAnterior->proximo = noPosterior;
+	}
+
+	// Deletar o nó e atualizar o contador
+	dadoNo = no->PontDados;
+	free(no);
+	(lista->cont)--;
+
+	return dadoNo; //retorna true	
+}
+
+void AdicionarNoOrdenado(Lista* lista, void* ponteiroDados){
+	// Cria e inicializa do nó
+	Node *novoNo = (Node*) malloc(sizeof(Node));
+	novoNo->PontDados = ponteiroDados;
+	novoNo->proximo = NULL;
+	(lista->cont)++;
+	
+	int tamLista = _contagemLista(lista);
+	Node* noAtual = lista->primeiroNo;
+	Node* noAnterior = NULL;
+
+	//Se a lista está vazia 
+	if(_contagemLista(lista) == 1){
+		 lista->primeiroNo = novoNo;	
+	} else {	
+	
+		for(int i = 1; i < tamLista; i++){
+			void* dadoComparar = (noAtual->PontDados);
+			// Usa a função de comparação para determinar onde inserir o novo nó
+			int retorno = (* lista->ComparaInserir)(ponteiroDados, dadoComparar);
+
+			// Inserir se novoNo > noAtual
+			if(retorno == 1){
+				// Se for primeiro nó, inserir no cabeçalho
+				if(i == 1){ 
+					Node* temp = lista->primeiroNo;
+					lista->primeiroNo = novoNo;
+					novoNo->proximo = temp;
+
+				} else {
+					Node* temp = noAnterior->proximo;
+					noAnterior->proximo = novoNo;
+					novoNo->proximo = temp;
+				}
+				return;
+			}
+			noAnterior = noAtual;
+			noAtual = noAtual->proximo;
+		}
+		// Inserir novoNo no final da lista se for menor (ou igual) ao último nó
+		noAnterior->proximo = novoNo;
+	}	
+	
+}
+
+// Executa a função passada no parâmetro em todos os nós
+void AtravessarLista(Lista* lista, void (*Funcao)(void* PtrDados)){
+	int tamLista = _contagemLista(lista);
+	Node* noAtual = lista->primeiroNo;
+	for (int i = 0; i < tamLista; i++){
+		// Executa a função em todos os nós
+		(*Funcao)(noAtual->PontDados);
+		noAtual = noAtual->proximo;
+	}
+}
+
+int _procurarPosicaoNo(Lista* lista, void* dadoBuscar){	
+	Node* noAtual = lista->primeiroNo;
+	Node* noAnterior = NULL;
+	int tamLista = _contagemLista(lista);
+
+	if(tamLista > 0){
+		for(int i = 1; i <= tamLista; i++){
+			void* dadoComparar = (noAtual->PontDados);
+			int retorno = (*lista->ComparaBusca)(dadoBuscar, dadoComparar);						
+			if(retorno == 0){
+				return i;
+			}
+			noAnterior = noAtual;
+			noAtual = noAtual->proximo;
+		}
+	} 
+	// Se não encontrar o nó procurado
+	return 0;
+} 
+
+Node* _procurarNoAnterior(Lista* lista, int posicao){
+	Node* noAtual = lista->primeiroNo;
+	Node* noAnterior = NULL;
+	for (int i = 1; i < posicao; i++){
+		noAnterior = noAtual;
+		noAtual = noAtual->proximo;
+	}	
+	return noAnterior;
+}
+
+Node* _procurarUltimoNo(Lista* lista){
+	int tamLista = _contagemLista(lista);
+	Node* noAnterior = _procurarNoAnterior(lista, tamLista);
+	if(noAnterior == NULL) //Se só tiver um nó na lista
+		return (lista->primeiroNo); 
+	else
+		return (noAnterior->proximo);
+}
+
+void _destruirNo(Node* no){
+	free(no->PontDados);
+	free(no);
+}
+
+
+int _listaVazia(Lista* lista){
+	if(_contagemLista(lista) == 0){
+		return 1;
+	} else{
+		return 0;
+	}
+}
+
+int _contagemLista(Lista* lista){
+	return lista->cont;
+}
+
